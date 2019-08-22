5f02bd8f3f1b5cc6d9c2d1fd6918abc344d45fc1
Alaa Hassan <macbookpro@MacBooks-MacBook-Pro.local>
2019-08-22 18:50:18 +0300

    third day

--- 3day.py
+++ 3day.py
@@ -2,3 +2,31 @@ x = 5
 y = "John"
 print(x)
 print(y)
+
+"""يمكن تغيير نوع المتغير حتى لو تم إنشائه، ولذلك بإسناد قيمة جديدة إليه بنوع مختلف.
+"""
+z=4
+z="sally"
+print(z)
+
+#➢ String variables can be declared either by using (single or double quotes ).
+a="alaa"
+print(a)
+a='alaa'
+print(a)
+
+#Assign Value to Multiple Variables
+o ,b ,c="Orange","Banana","Cherry"
+print(o)
+print(b)
+print(c)
+
+#➢ And you can assign the same value to multiple variables in one line.
+s=d=p="Alaa"
+print(s)
+print(d)
+print(p)
+
+#use + to combine statement in output
+v = "Alaa"
+print("my name is " + v )
