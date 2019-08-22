b09cb6eb63c7d474fbe5986871be21a642ca69b9
Alaa Hassan <macbookpro@MacBooks-MacBook-Pro.local>
2019-08-22 14:29:01 +0300

    second day

---
+++ 2day.py
@@ -0,0 +1,13 @@
+print("Hello,World") #هذا تعليق بالبايثون
+#print("Hello,World")
+print("cheers,mate!")
+#aaaaa
+#bbbbb
+#ccccc
+"""
+Since Python will ignore string literals that are not 
+assigned to a variable, you can add a multiline string 
+(triple quotes) in your code and place your comment inside it.
+"""
+
+
