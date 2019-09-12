7ff2d3171cddfd054f12bed5329637dc30b6e034
AlaaHassaan <AlaaHassanCV@gmail.com>
2019-09-12 13:45:28 +0300

    7 day

--- 7day.py
+++ 7day.py
@@ -3,12 +3,13 @@ print('Hello')
 a="hello"
 print(a)
 #Multiline Strings
-b='''
-You can assign a multiline string to a 
-variable by using three quotes.
-You can use three double quotes (""") 
-Or three single quotes '''
+b=''' Alaa
+python
+programming'''
+
 print(b)
 #Strings are Arrays
+a="hello  world"
 print(a[1])
 print(a[2:5])
+print(a[0:6])
